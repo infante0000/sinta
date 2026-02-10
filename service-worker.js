338b52d77dda17906ee13f639ecc7a8a78b06328
Niña Infante <86414019+infante0000@users.noreply.github.com>
2026-02-10 14:16:06 +0000

    prepare for downloadable tab

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,9 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
